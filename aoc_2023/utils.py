32c810de6b3995e2995cfd8646c7026b43da1420
SimeonHristov99 <s.e.hristov99@gmail.com>
2023-12-02 18:52:43 +0200

    Add new utility function.

--- aoc_2023/utils.py
+++ aoc_2023/utils.py
@@ -1,3 +1,18 @@
+def remove_punctuation(input: str, characters_to_remove: set[str] = {',', '.', '!', '?', ';', ':'}) -> str:
+    """
+    Removes a set of characters from an input string.
+
+    Args:
+        input (str): The string from which to remove the characters.
+        characters_to_remove (set[str], optional): The characters to remove from `input`. Defaults to {',', '.', '!', '?', ';', ':'}.
+
+    Returns:
+        str: A string in which there are no characters that are present in `characters_to_remove`.
+    """
+    result = ''.join([char for char in input if char not in characters_to_remove])
+    return result
+
+
 def get_lines(filename: str) -> list[str]:
     """
     Parses the lines of a text file.
