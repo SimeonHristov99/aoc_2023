278455eeed5f38976bd55cfd1e1f69a93dad00ea
SimeonHristov99 <s.e.hristov99@gmail.com>
2023-12-02 16:03:22 +0200

    Add template day.

---
+++ aoc_2023/day_template/main.py
@@ -0,0 +1,18 @@
+def part1(filename: str) -> int:
+    return 42
+
+
+def part2(filename: str) -> int:
+    return 42
+
+
+def main() -> None:
+    print(f'Part 1, Sample: {part1("aoc_2023/day01/sample1.txt")}')
+    print(f'Part 1, Input: {part1("aoc_2023/day01/input.txt")}')
+
+    print(f'Part 2, Sample: {part2("aoc_2023/day01/sample2.txt")}')
+    print(f'Part 2, Input: {part2("aoc_2023/day01/input.txt")}')
+
+
+if __name__ == '__main__':
+    main()
