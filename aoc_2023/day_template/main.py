6e9b8706122a7005b0cc06d4d1ec71ef4ef33f4b
SimeonHristov99 <s.e.hristov99@gmail.com>
2023-12-02 16:06:05 +0200

    Update filenames.

--- aoc_2023/day_template/main.py
+++ aoc_2023/day_template/main.py
@@ -7,11 +7,11 @@ def part2(filename: str) -> int:
 
 
 def main() -> None:
-    print(f'Part 1, Sample: {part1("aoc_2023/day01/sample1.txt")}')
-    print(f'Part 1, Input: {part1("aoc_2023/day01/input.txt")}')
+    print(f'Part 1, Sample: {part1("aoc_2023/day_template/sample.txt")}')
+    print(f'Part 1, Input: {part1("aoc_2023/day_template/input.txt")}')
 
-    print(f'Part 2, Sample: {part2("aoc_2023/day01/sample2.txt")}')
-    print(f'Part 2, Input: {part2("aoc_2023/day01/input.txt")}')
+    print(f'Part 2, Sample: {part2("aoc_2023/day_template/sample.txt")}')
+    print(f'Part 2, Input: {part2("aoc_2023/day_template/input.txt")}')
 
 
 if __name__ == '__main__':
