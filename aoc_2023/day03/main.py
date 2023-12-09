2f9b39622594ddc2b0b7f826b8a631cc9093aaa9
SimeonHristov99 <s.e.hristov99@gmail.com>
2023-12-09 18:41:22 +0200

    Lint code.

--- aoc_2023/day03/main.py
+++ aoc_2023/day03/main.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import math
+
 from aoc_2023 import utils
 
 
@@ -56,17 +58,40 @@ def part1(filename: str) -> int:
 
 
 def part2(filename: str) -> int:
-    return 42
+    schematic = utils.get_lines(filename)
+
+    n_rows = len(schematic)
+    n_cols = len(schematic[0])
+
+    visited: set[tuple[int, int]] = set()
+    result = 0
+
+    for i in range(n_rows):
+        for j in range(n_cols):
+            if schematic[i][j] == '*':
+                numbers = []
+                for ii in range(-1, 2):
+                    for jj in range(-1, 2):
+                        coords = (i + ii, j + jj)
+                        if (
+                            utils.valid_coords(i + ii, j + jj, schematic)
+                            and schematic[i + ii][j + jj].isnumeric()
+                            and coords not in visited
+                        ):
+                            number = extract_number(schematic, visited, coords)
+                            numbers.append(number)
+                if len(numbers) == 2:
+                    result += math.prod(numbers)
+
+    return result
 
 
 def main() -> None:
     print(f'Part 1, Sample: {part1("aoc_2023/day03/sample.txt")}')
     print(f'Part 1, Input: {part1("aoc_2023/day03/input.txt")}')
 
-    utils.valid_coords(0, 1, [[1, 2, 3]])
-
-    # print(f'Part 2, Sample: {part2("aoc_2023/day03/sample.txt")}')
-    # print(f'Part 2, Input: {part2("aoc_2023/day03/input.txt")}')
+    print(f'Part 2, Sample: {part2("aoc_2023/day03/sample.txt")}')
+    print(f'Part 2, Input: {part2("aoc_2023/day03/input.txt")}')
 
 
 if __name__ == '__main__':
