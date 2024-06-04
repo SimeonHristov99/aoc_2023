e6da02789a1aa702c0caf315e8232cf7707891db
SimeonHristov99 <s.e.hristov99@gmail.com>
2024-06-04 23:09:34 +0300

    Add non-optimial solution to part 2.

--- aoc_2023/day05/main.py
+++ aoc_2023/day05/main.py
@@ -46,11 +46,28 @@ def part1(filename: str) -> int:
     return int(min_loc)
 
 
+def part2(filename: str) -> int:
+    lines = utils.get_lines(filename)
+    size = 2
+
+    seeds = parse_numbers(lines[0].split(': ')[-1])
+    _, mappings = get_seeds_mappings(utils.get_lines(filename))
+
+    ranges = [tuple(seeds[i:i+size]) for i in range(0, len(seeds), size)]
+
+    min_loc = float('inf')
+    for rang_start, rang_size in ranges:
+        for seed in range(rang_start, rang_start+rang_size):
+            min_loc = min(min_loc, get_location(mappings, seed))
+
+    return int(min_loc)
+
+
 def main() -> None:
     print(f'Part 1, Sample: {part1("aoc_2023/day05/sample.txt")}')  # 35
     print(f'Part 1, Input: {part1("aoc_2023/day05/input.txt")}')  # 323142486
 
-    # print(f'Part 2, Sample: {part2("aoc_2023/day05/sample.txt")}')
+    print(f'Part 2, Sample: {part2("aoc_2023/day05/sample.txt")}')  # 46
     # print(f'Part 2, Input: {part2("aoc_2023/day05/input.txt")}')
 
 
