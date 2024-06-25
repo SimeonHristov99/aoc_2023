64c738deeb61d2158609c13e6a8eefdef4efd3d8
SimeonHristov99 <s.e.hristov99@gmail.com>
2024-06-25 10:34:17 +0300

    Make tests pass. Use new code for part 1.

--- aoc_2023/day05/main.py
+++ aoc_2023/day05/main.py
@@ -31,78 +31,13 @@ def get_seeds_and_maps(lines: list[str]):
             continue
         elif len(line) > 0:
             destination_start, source_start, length = parse_numbers(line)
-            current_map[source_start] = (destination_start, length)
+            current_map[destination_start] = (source_start, length)
         elif len(line) == 0:
-            current_map = dict(
-                sorted(current_map.items(), key=lambda kv: kv[0]),
-            )
+            current_map = dict(current_map.items())
             maps.append(current_map)
             current_map = {}
 
     return seeds, maps
-
-
-def get(map, seed):
-    if seed in map:
-        return map[seed][0]
-
-    c_map_ks = list(map.keys())
-    for k1, k2 in zip(c_map_ks, c_map_ks[1:]):
-        if k1 <= seed <= k2:
-            destination_start, length = map[k1]
-            if seed <= k1 + length:
-                return destination_start + seed - k1
-            return seed
-
-    if k2 <= seed <= k2 + map[k2][1]:
-        return map[k2][0] + seed - k2
-    return seed
-
-
-def part1(filename: str) -> int:
-    seeds, maps = get_seeds_and_maps(get_lines(filename))
-
-    locations = []
-
-    for seed in seeds:
-        for c_map in maps:
-            seed = get(c_map, seed)
-
-        locations.append(seed)
-
-    return min(locations)
-
-
-def step(to_process, line_start, line_end):
-    diff = line_end[0] - line_start[0]
-
-    if line_start[0] <= to_process[0] < to_process[1] <= line_start[1]:
-        # interval contains source line
-        done = (to_process[0] + diff, to_process[1] + diff)
-        to_process = []
-        return (done, to_process)
-
-    if line_start[0] <= to_process[0]:
-        # interval contains left part of source line, but not end
-        done = (to_process[0] + diff, line_start[1] + diff)
-        to_process = (line_start[1] + 1, to_process[1])
-        return (done, to_process)
-
-    if line_start[0] <= to_process[1]:
-        # interval contains right part of source line, but not start
-        done = (line_start[0] + diff, to_process[1] + diff)
-        to_process = (to_process[0], line_start[0] - 1)
-        return (done, to_process)
-
-    if to_process[0] <= line_start[0] < line_start[1] <= to_process[1]:
-        # interval is contained in source line
-        to_process_before = (to_process[0], line_start[0] - 1)
-        done_on_line = (line_start[0] + diff, line_start[1] + diff)
-        to_process_after = (line_start[1] + 1, to_process[1])
-        return (done_on_line, [to_process_before, to_process_after])
-
-    # interval does not contained any part of source line
-    return (None, [to_process])
 
 
 def move(
@@ -204,38 +139,48 @@ def almanac(maps: list[dict[int, tuple[int, int]]],
     return done
 
 
-def part2(filename: str) -> int:
-    seed_ranges, maps = get_seeds_and_maps(get_lines(filename))
-    it = iter(seed_ranges)
-    ranges = list(zip(it, it))
+def part1(filename: str) -> int:
+    seeds, maps = get_seeds_and_maps(get_lines(filename))
+    acc = float('inf')
 
-    for range in ranges:
-        to_process = [range]
+    for seed in seeds:
+        acc = min(acc, almanac(maps, (seed, seed)).pop()[0])
 
-        for map in maps:
-            dones = []
+    return int(acc)
 
-            for dest_start, (source_start, length) in map.items():
-                to_process_new = []
-                for to_process_current in to_process:
-                    done, to_pr = step(
-                        to_process_current, source_start,
-                        source_start + length - 1
-                    )
-                    to_process_new.append(to_pr)
-                    if done:
-                        dones.append(done)
 
-            to_process.extend(dones)
+# def part2(filename: str) -> int:
+#     seed_ranges, maps = get_seeds_and_maps(get_lines(filename))
+#     it = iter(seed_ranges)
+#     ranges = list(zip(it, it))
 
-    return 42
+#     for range in ranges:
+#         to_process = [range]
+
+#         for map in maps:
+#             dones = []
+
+#             for dest_start, (source_start, length) in map.items():
+#                 to_process_new = []
+#                 for to_process_current in to_process:
+#                     done, to_pr = step(
+#                         to_process_current, source_start,
+#                         source_start + length - 1
+#                     )
+#                     to_process_new.append(to_pr)
+#                     if done:
+#                         dones.append(done)
+
+#             to_process.extend(dones)
+
+#     return 42
 
 
 def main() -> None:
-    # print(f'Part 1, Sample: {part1("aoc_2023/day05/sample.txt")}')  # 35
-    # print(f'Part 1, Input: {part1("aoc_2023/day05/input.txt")}')  # 323142486
+    print(f'Part 1, Sample: {part1("aoc_2023/day05/sample.txt")}')  # 35
+    print(f'Part 1, Input: {part1("aoc_2023/day05/input.txt")}')  # 323142486
 
-    print(f'Part 2, Sample: {part2("aoc_2023/day05/sample.txt")}')  # 46
+    # print(f'Part 2, Sample: {part2("aoc_2023/day05/sample.txt")}')  # 46
     # print(f'Part 2, Input: {part2("aoc_2023/day05/input.txt")}')
 
 
