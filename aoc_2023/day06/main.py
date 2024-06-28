8f0d9b3d042ffd89f3debda22b4c01b03d2bac26
SimeonHristov99 <s.e.hristov99@gmail.com>
2024-06-28 08:38:43 +0300

    Add skeleton for day 06.

---
+++ aoc_2023/day06/main.py
@@ -0,0 +1,20 @@
+def part1(filename: str) -> int:
+    print('Hello, world')
+    return 42
+
+
+def part2(filename: str) -> int:
+    print('Hello, world')
+    return 42
+
+
+def main() -> None:
+    print(f'Part 1, Sample: {part1("aoc_2023/day05/sample.txt")}')  # 35
+    # print(f'Part 1, Input: {part1("aoc_2023/day05/input.txt")}')
+
+    # print(f'Part 2, Sample: {part2("aoc_2023/day05/sample.txt")}')
+    # print(f'Part 2, Input: {part2("aoc_2023/day05/input.txt")}')
+
+
+if __name__ == '__main__':
+    main()
