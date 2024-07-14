5b7a9e1e19ead9ec10b0d11155e5d215f5e5839b
SimeonHristov99 <s.e.hristov99@gmail.com>
2024-07-14 08:58:46 +0300

    Add failing tests.

--- tests/test_day09.py
+++ tests/test_day09.py
@@ -62,6 +62,48 @@ class TestDay09(unittest.TestCase):
         # Assert
         self.assertEqual(actual, expected)
 
+    def test_predict_left_v1(self):
+        """
+        Tests that predicting the first number works - variant 1.
+        """
+        # Arrange
+        xs = [-3, 0, 3, 6, 9, 12, 15]
+        expected = 18
+
+        # Act
+        actual = main.predict(xs)
+
+        # Assert
+        self.assertEqual(actual, expected)
+
+    def test_predict_left_v2(self):
+        """
+        Tests that predicting the first number works - variant 2.
+        """
+        # Arrange
+        expected = 28
+        xs = [0, 1, 3, 6, 10, 15, 21]
+
+        # Act
+        actual = main.predict(xs)
+
+        # Assert
+        self.assertEqual(actual, expected)
+
+    def test_predict_left_v3(self):
+        """
+        Tests that predicting the first number works - variant 3.
+        """
+        # Arrange
+        xs = [10, 13, 16, 21, 30, 45]
+        expected = 5
+
+        # Act
+        actual = main.predict(xs, left=True)
+
+        # Assert
+        self.assertEqual(actual, expected)
+
     def test_part1_sample(self):
         """
         Tests that passing the sample for part 1 works.
