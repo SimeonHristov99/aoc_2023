3a4282e5405d33547ca93fb8ed009bd836ae880b
SimeonHristov99 <s.e.hristov99@gmail.com>
2024-07-16 10:03:04 +0300

    Make tests for  pass.

--- aoc_2023/day10/main.py
+++ aoc_2023/day10/main.py
@@ -8,6 +8,68 @@ def find_one(matrix: list[list[str]]) -> tuple[int, int]:
     raise NotImplementedError('Coordinates of starting point not found.')
 
 
+def up(
+    matrix: list[list[str]], coords: tuple[int, int],
+    stack: list[list[tuple[int, int]]], seen: set[tuple[int, int]],
+    path: list[tuple[int, int]]
+) -> list[list[tuple[int, int]]]:
+    x, y = coords
+    if 0 < x:
+        next_ = (x - 1, y)
+        if next_ not in seen:
+            stack.append(path + [next_])
+    return stack
+
+
+def num_steps_farthest(
+    matrix: list[list[str]], start_coords: tuple[int, int]
+) -> int:
+    num_rows = len(matrix)
+    num_cols = len(matrix[0])
+    stack = [[start_coords]]
+    seen = set()
+    max_len_path = 0
+
+    while len(stack) > 0:
+        path = stack.pop()
+        x, y = path[-1]
+
+        max_len_path = max(max_len_path, len(path))
+
+        seen.add((x, y))
+
+        if matrix[x][y] == '|':  # only up and down
+            ...
+        elif matrix[x][y] == '-':  # only left and right
+            ...
+        elif matrix[x][y] == 'L':  # only up and right
+            ...
+        elif matrix[x][y] == 'J':  # only up and left
+            ...
+        elif matrix[x][y] == '7':  # only down and left
+            ...
+        elif matrix[x][y] == 'F':  # only down and right
+            ...
+        else:
+            continue
+
+        if 0 < y:
+            next_ = (x, y - 1)  # left
+            if next_ not in seen:
+                stack.append(path + [next_])
+        if y < num_cols - 1:
+            next_ = (x, y + 1)  # right
+            if next_ not in seen:
+                stack.append(path + [next_])
+
+        if x < num_rows - 1:
+            next_ = (x + 1, y)  # down
+            if next_ not in seen:
+                stack.append(path + [next_])
+
+    return max_len_path
+
+
 def part1(filename: str) -> int:
     return 42
 
