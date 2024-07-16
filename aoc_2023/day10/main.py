cae8287905990a1e7ef2406475015e8cea18dd75
SimeonHristov99 <s.e.hristov99@gmail.com>
2024-07-16 11:47:41 +0300

    Make test pass.

--- aoc_2023/day10/main.py
+++ aoc_2023/day10/main.py
@@ -64,6 +64,10 @@ def initialize_stack(
     return result
 
 
+def manhattan_dist(p1: tuple[int, int], p2: tuple[int, int]) -> int:
+    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+
+
 def num_steps_farthest(
     matrix: list[list[str]], start_coords: tuple[int, int]
 ) -> int:
