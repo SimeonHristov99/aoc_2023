b654b382c85f6969b6d3107203be97ecb1743cee
SimeonHristov99 <s.e.hristov99@gmail.com>
2024-10-05 17:27:01 +0300

    Add failing tests.

--- tests/test_day10_new.py
+++ tests/test_day10_new.py
@@ -19,17 +19,39 @@ class TestDay10New(unittest.TestCase):
             ['L', '|', '7', '|', '|'],
             ['-', 'L', '-', 'J', '|'],
             ['L', '|', '-', 'J', 'F'],
-        ], [['.', '.', '.', '.', '.'],
-            ['.', 'S', '-', '7', '.'],
-            ['.', '|', '.', '|', '.'],
-            ['.', 'L', '-', 'J', '.'],
-            ['.', '.', '.', '.', '.']]
-        ]
+        ],
+                     [['.', '.', '.', '.', '.'], ['.', 'S', '-', '7', '.'],
+                      ['.', '|', '.', '|', '.'], ['.', 'L', '-', 'J', '.'],
+                      ['.', '.', '.', '.', '.']]]
         actuals = []
 
         # Act
         for input_file in filenames:
             actuals.append(main.parse_input(input_file))
+
+        # Assert
+        self.assertListEqual(actuals, expecteds)
+
+    def test_find_start(self):
+        """
+        Tests that the symbol, marking the starting pipe - S, is found correctly.
+        """
+        # Arrange
+        inputs = [[
+            ['-', 'L', '|', 'F', '7'],
+            ['7', 'S', '-', '7', '|'],
+            ['L', '|', '7', '|', '|'],
+            ['-', 'L', '-', 'J', '|'],
+            ['L', '|', '-', 'J', 'F'],
+        ],
+                  [['.', '.', 'F', '7', '.'], ['.', 'F', 'J', '|', '.'], ['S', 'J', '.', 'L', '7'],
+                   ['|', 'F', '-', '-', 'J'], ['L', 'J', '.', '.', '.']]]
+        expecteds = [(1, 1), (2, 0)]
+        actuals = []
+
+        # Act
+        for input_map in inputs:
+            actuals.append(main.find_start(input_map))
 
         # Assert
         self.assertListEqual(actuals, expecteds)
