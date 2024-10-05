ae377e04ac0b9b2c6021f9634d58c8703c379d96
SimeonHristov99 <s.e.hristov99@gmail.com>
2024-10-05 17:17:25 +0300

    Start clean.

--- aoc_2023/day10_new/main.py
+++ aoc_2023/day10_new/main.py
@@ -1,5 +1,24 @@
+from typing import List
+
+def parse_input(filename: str) -> List[List[str]]:
+    with open(filename, 'r') as f:
+        lines = f.read().splitlines()
+    return [[c for c in line] for line in lines]
+
+def part1(filename: str) -> int:
+    return 42
+
+
+def part2(filename: str) -> int:
+    return 42
+
+
 def main() -> None:
-    print('Hello, world')
+    print(f'Part 1, Sample: {part1("sample.txt")}')
+    print(f'Part 1, Input: {part1("input.txt")}')
+
+    print(f'Part 2, Sample: {part2("sample.txt")}')
+    print(f'Part 2, Input: {part2("input.txt")}')
 
 
 if __name__ == '__main__':
