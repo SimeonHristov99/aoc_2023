6f2b527940f7a845219c6b88b8d16a240b8ce247
SimeonHristov99 <s.e.hristov99@gmail.com>
2024-12-14 16:18:37 +0200

    Make tests pass.

--- aoc_2023/day12/main.py
+++ aoc_2023/day12/main.py
@@ -8,5 +8,15 @@ def parse(filename: str) -> list[tuple[list[str], list[int]]]:
     return result
 
 
+def create_combinations(num_broken, result, idx_start, idx_current, it_num_broken,
+                        it_pattern) -> int:
+    next_group = next(it_num_broken, None)
+    if not next_group and idx_current > len(num_broken):
+        return 1
+    if next_group and idx_current > len(num_broken):
+        return 0
+    return 2
+
+
 def get_num_combinations(pattern: list[str], num_broken: list[int]) -> int:
     return 1
