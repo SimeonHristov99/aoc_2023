a0cb10f28e2e20bb297a0f4c10a9aa6232785e8a
SimeonHristov99 <s.e.hristov99@gmail.com>
2024-12-14 16:02:55 +0200

    Make test pass.

--- aoc_2023/day12/main.py
+++ aoc_2023/day12/main.py
@@ -6,3 +6,7 @@ def parse(filename: str) -> list[tuple[list[str], list[int]]]:
         input_string, groups = line.split()
         result.append((list(input_string), [int(grp) for grp in groups.split(',')]))
     return result
+
+
+def get_num_combinations(pattern: list[str], num_broken: list[int]) -> int:
+    return 1
