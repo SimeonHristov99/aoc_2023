f09ff2b16bd73de158d63f6ce1e22fe70eed09bd
SimeonHristov99 <s.e.hristov99@gmail.com>
2024-12-20 16:26:46 +0200

    Change return type of parse.

--- aoc_2023/day12/main.py
+++ aoc_2023/day12/main.py
@@ -1,10 +1,10 @@
-def parse(filename: str) -> list[tuple[list[str], list[int]]]:
+def parse(filename: str) -> list[tuple[str, list[int]]]:
     with open(filename, 'r') as fp:
         lines = fp.read().splitlines()
     result = []
     for line in lines:
         input_string, groups = line.split()
-        result.append((list(input_string), [int(grp) for grp in groups.split(',')]))
+        result.append((input_string, [int(grp) for grp in groups.split(',')]))
     return result
 
 
