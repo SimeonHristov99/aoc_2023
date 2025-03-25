986df8029aca0100344a0f6fbe9dde94e3f175ea
SimeonHristov99 <s.e.hristov99@gmail.com>
2025-03-25 09:21:40 +0200

    Checkpoint.

--- aoc_2023/day12/main.py
+++ aoc_2023/day12/main.py
@@ -7,8 +7,27 @@ def parse(filename: str) -> list[tuple[str, list[int]]]:
         result.append((input_string, [int(grp) for grp in groups.split(',')]))
     return result
 
-def get_num_combinations(pattern: str, num_broken: list[int]):
-    return 1
+
+def get_num_combinations(pattern: str, num_broken: list[int]) -> int:
+    if pattern == '':
+        return 1 if num_broken == [] else 0
+
+    if num_broken == []:
+        return 0 if '#' in pattern else 1
+
+    count = 0
+
+    if pattern[0] == '.' or pattern[0] == '?':
+        # treating the '?' as '.'
+        count += get_num_combinations(pattern[1:], num_broken)
+
+    if ((pattern[0] == '#' or pattern[0] == '?') and len(pattern) >= num_broken[0]
+            and '.' not in pattern[:num_broken[0]]
+            and (len(pattern) == num_broken[0] or pattern[num_broken[0]] != '#')):
+        # treating the '?' as '#'
+        count += get_num_combinations(pattern[num_broken[0] + 1:], num_broken[1:])
+
+    return count
 
 
 def part1(filename: str) -> int:
