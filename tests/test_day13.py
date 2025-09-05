0c13ac5dd8e27d9e87bbbb7d8ba0d8bfa0daee21
SimeonHristov99 <s.e.hristov99@gmail.com>
2025-09-05 13:13:40 +0300

    Add failing test: when_two_patterns_passed_then_returns_two_lists_each_with_one_pattern.

--- tests/test_day13.py
+++ tests/test_day13.py
@@ -1,6 +1,38 @@
+import os
 import unittest
 
 from aoc_2023.day13 import main
+
+
+class TestParseInput(unittest.TestCase):
+
+    def test_when_two_patterns_passed_then_returns_two_lists_each_with_one_pattern(self):
+        # Arrange
+        filename = os.path.join('aoc_2023', 'day13', 'sample.txt')
+        expected = [[
+            '#.##..##.',
+            '..#.##.#.',
+            '##......#',
+            '##......#',
+            '..#.##.#.',
+            '..##..##.',
+            '#.#.##.#.',
+        ],
+                    [
+                        '#...##..#',
+                        '#....#..#',
+                        '..##..###',
+                        '#####.##.',
+                        '#####.##.',
+                        '..##..###',
+                        '#....#..#',
+                    ]]
+
+        # Act
+        actual = main.parse_input(filename)
+
+        # Assert
+        self.assertListEqual(actual, expected, f'\n{actual=}\n{expected=}\n')
 
 
 class TestPart1(unittest.TestCase):
