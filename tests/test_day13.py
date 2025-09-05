f33c59bae05572eaa4b28931f458743bed44a25e
SimeonHristov99 <s.e.hristov99@gmail.com>
2025-09-05 17:37:47 +0300

    Make tests fail by requiring distribution logic.

--- tests/test_day13.py
+++ tests/test_day13.py
@@ -183,6 +183,14 @@ class TestSummarizerSummarizeDirection(unittest.TestCase):
             '#.#.##.#.',
         ]
         summarizer = main.Summarizer(pattern)
+        summarizer.lines_horizontal = {
+            0: [(0, 1)],
+            1: [(1, 2), (0, 3)],
+            2: [(2, 3), (1, 4), (0, 5)],
+            3: [(3, 4), (2, 5), (1, 6)],
+            4: [(4, 5), (3, 6)],
+            5: [(5, 6)]
+        }
 
         # Act
         actual = summarizer.summarize_direction(direction)
@@ -204,6 +212,16 @@ class TestSummarizerSummarizeDirection(unittest.TestCase):
             '#.#.##.#.',
         ]
         summarizer = main.Summarizer(pattern)
+        summarizer.lines_vertical = {
+            0: [(0, 1)],
+            1: [(1, 2), (0, 3)],
+            2: [(2, 3), (1, 4), (0, 5)],
+            3: [(3, 4), (2, 5), (1, 6), (0, 7)],
+            4: [(4, 5), (3, 6), (2, 7), (1, 8)],
+            5: [(5, 6), (4, 7), (3, 8)],
+            6: [(6, 7), (5, 8)],
+            7: [(7, 8)]
+        }
 
         # Act
         actual = summarizer.summarize_direction(direction)
