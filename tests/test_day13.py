6b5fd9d0f26a1e97e6dd322c5c8c25af7ccec56e
SimeonHristov99 <s.e.hristov99@gmail.com>
2025-09-05 14:13:48 +0300

    Add failing test: when_called_on_rows_then_returns_zero.

--- tests/test_day13.py
+++ tests/test_day13.py
@@ -79,6 +79,30 @@ class TestSummarizerCreateReflectionMaps(unittest.TestCase):
         self.assertTupleEqual(actual, expected)
 
 
+class TestSummarizerSummarizeDirection(unittest.TestCase):
+
+    def test_when_called_on_rows_then_returns_zero(self):
+        # Arrange
+        expected = 0
+        direction = 'rows'
+        pattern = [
+            '#.##..##.',
+            '..#.##.#.',
+            '##......#',
+            '##......#',
+            '..#.##.#.',
+            '..##..##.',
+            '#.#.##.#.',
+        ]
+        summarizer = main.Summarizer(pattern)
+
+        # Act
+        actual = summarizer.summarize_direction(direction)
+
+        # Assert
+        self.assertEqual(actual, expected)
+
+
 class TestSummarizerSummarize(unittest.TestCase):
 
     def test_when_called_then_return_zero(self):
